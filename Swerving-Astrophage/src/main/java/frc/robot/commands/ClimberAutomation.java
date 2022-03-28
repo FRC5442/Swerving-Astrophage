c7ba06e32bec6df58300c0a1070cd76d130765ab
Devdatta Kasture <dakasture11@gmail.com>
2022-03-28 09:56:44 -0500

    adding backspin motor, climber automation - Noah

--- Swerving-Astrophage/src/main/java/frc/robot/commands/ClimberAutomation.java
+++ Swerving-Astrophage/src/main/java/frc/robot/commands/ClimberAutomation.java
@@ -4,7 +4,10 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
 
@@ -18,9 +21,42 @@ public class ClimberAutomation extends SequentialCommandGroup {
     // addCommands(new FooCommand(), new BarCommand());
     // ClimberCommand pivotLeft = RobotContainer.pivotLeftCommand;
     // ClimberCommand reversePivotLeft = RobotContainer.reversePivotLeftCommand;
+    final ClimberCommand moveLeftForward = new ClimberCommand(RobotContainer.pivotLeft, -Constants.ClimberConstants.PIVOT_FRONT_POSITION, 0, -Constants.ClimberConstants.PIVOT_SPEED);
+    final ClimberCommand moveLeftBackward = new ClimberCommand(RobotContainer.pivotLeft, -Constants.ClimberConstants.PIVOT_FRONT_POSITION, 0, Constants.ClimberConstants.PIVOT_SPEED);
+
+    final ClimberCommand moveRightForward = new ClimberCommand(RobotContainer.pivotRight, 0, Constants.ClimberConstants.PIVOT_FRONT_POSITION, Constants.ClimberConstants.PIVOT_SPEED);
+    final ClimberCommand moveRightBackward = new ClimberCommand(RobotContainer.pivotRight, 0, Constants.ClimberConstants.PIVOT_FRONT_POSITION, -Constants.ClimberConstants.PIVOT_SPEED);
+
+    final ClimberCommand raiseLeftWinch = new ClimberCommand(RobotContainer.winchLeft, -Constants.ClimberConstants.WINCH_HIGH_POSITION, 0, -Constants.ClimberConstants.WINCH_SPEED);
+    final ClimberCommand lowerLeftWinch = new ClimberCommand(RobotContainer.winchLeft, -Constants.ClimberConstants.WINCH_HIGH_POSITION, 0, Constants.ClimberConstants.WINCH_SPEED);
+
+    final ClimberCommand raiseRightWinch = new ClimberCommand(RobotContainer.winchRight, 0, Constants.ClimberConstants.WINCH_HIGH_POSITION, Constants.ClimberConstants.WINCH_SPEED);
+    final ClimberCommand lowerRightWinch = new ClimberCommand(RobotContainer.winchRight, 0, Constants.ClimberConstants.WINCH_HIGH_POSITION, -Constants.ClimberConstants.WINCH_SPEED);
+
+
+
+
+
+
     addCommands(
-      new ClimberCommand(RobotContainer.pivotLeft, Constants.ClimberConstants.PIVOT_REAR_POSITION, Constants.ClimberConstants.PIVOT_FRONT_POSITION, Constants.ClimberConstants.PIVOT_SPEED),
-      new ClimberCommand(RobotContainer.pivotLeft, Constants.ClimberConstants.PIVOT_REAR_POSITION, Constants.ClimberConstants.PIVOT_FRONT_POSITION, -Constants.ClimberConstants.PIVOT_SPEED)
+      // Bring Both arms forward
+      new ParallelCommandGroup(
+        moveLeftForward,
+        moveRightForward),
+
+      // Extend both arms
+      new ParallelCommandGroup(
+        raiseLeftWinch,
+        raiseRightWinch),
+
+      // Pivot Right Arm to Bar
+      new ClimberCommand(RobotContainer.pivotRight, 0 + (Constants.ClimberConstants.PIVOT_FRONT_POSITION / 2), Constants.ClimberConstants.PIVOT_FRONT_POSITION, -Constants.ClimberConstants.PIVOT_SPEED),
+        
+      // lower right winch
+      lowerRightWinch,
+
+      // Wait at the end to avoid restarting the process:
+      new WaitCommand(10)
     );
   }
 }
